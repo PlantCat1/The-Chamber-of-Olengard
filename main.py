0ebe2666f378298a660fd84c80ae3f52f9067b79
PlantCat1 <danielmaloney03@gmail.com>
2021-06-01 13:18:02 -0700

    fixes

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
+from text import *
+
 def start_adventure():
     print("")
-    print("You find yourself in a flat plain with swaying green grass and massive oak trees scattered near and far into the distance.")
-    print("As you look around, you see no signs of civliation, only plains as far as the eye can seeBehind you is a small hill with tall,")
-    print("light green grasses and shrubs swaying in the light, cool wind. On the edge of the hill is a small, deteriorated platform with a double door going into it")
-    print("You do not remember where you are or how you got here. amd there is a weird sense of urgancy to the situation, specically regrading the doors going into the hill.")
+    print(text1)
     print("")
     print("Having no idea where you are or why you are here, there are few options for what to do. You can either (1) investiage the platform or (2) investigate the surrounding area")
-    platform_or_area = float(input("> "))
-    if platform_or_area == 1:
-        #platform stuff
-        print("platform")
-    elif platform_or_area == 2:
-        #surroding area stuff
-        print("area")
+    platform_or_area = input("> ")
+    if platform_or_area >= "1" and platform_or_area <= "1":
+        print("")
+        print(text2)
+        print("")
+    elif platform_or_area >= "2" and platform_or_area <= "2":
+        #print("")
+        #print("text3")
+        print("")
     else:
         print("Sorry, you can only answer 1 or 2")
         while True:
-            platform_or_area = float(input("> "))
-            if platform_or_area == 1:
-                 #platform stuff
-                 print("platform1")
+            platform_or_area = input("> ")
+            if platform_or_area >= "1" and platform_or_area <= "1":
+                 print("")
+                 print(text2)
+                 print("")
                  break
-            elif platform_or_area == 2:
-                 #surroding area stuff
-                 print("area1")
+            elif platform_or_area >= "2" and platform_or_area <= "2":
+                 #print("")
+                 #print("text3")
+                 print("")
                  break
             else:
                 print("Come again?")
@@ -33,24 +36,28 @@ def intro():
     player_name = input("> ")
     print("I see, " + player_name + ".")
     print("")
-    print("One last question before the adventure begins... Are you more of a bow and arrow or sword type?")
+    print("One last question before the adventure begins... Are you more of a (1) bow and arrow or (2) a sword type?")
     player_weapon = input("> ")
-    if "sword" in player_weapon.lower():
+    if player_weapon >= "2" and player_weapon <= "2":
         player_weapon = True
-    elif player_weapon.lower() in ["bow", "arrow", "bow and arrow"]:
+    elif player_weapon >= "1" and player_weapon <= "1":
         player_weapon = False
     else:
-        print("Sorry, I do not understand this. Please say again?")
+        print("Sorry, I do not understand this. Please try again.")
         while True:
             player_weapon = input("> ")
-            if "sword" in player_weapon.lower():
+            if player_weapon >= "2" and player_weapon <= "2":
                  player_weapon = True
                  break
-            elif player_weapon.lower() in ["bow", "arrow", "bow and arrow"]:
+            if player_weapon >= "2" and player_weapon <= "2":
                  player_weapon = False
                  break
             else:
                 print("Come again?")
+    if player_weapon:
+        print("Ah I see... I thought you would pick the sword")
+    else:
+        print("Ah I see... I thought you would pick the bow")
     start_adventure()
 
 intro()
---
+++ text.py
@@ -0,0 +1,8 @@
+text1 = """You find yourself in a flat plain with swaying green grass and massive oak trees scattered near and far into the distance.
+As you look around, you see no signs of civilization, only plains as far as the eye can see. Behind you is a small hill with tall,
+light green grasses and shrubs swaying in the light, wind. On the edge of the hill is a small, deteriorated platform with a double door going into it
+You do not remember where you are or how you got here, but there is a weird sense of urgency, specifically regarding the doors going into the hill."""
+
+text2 = """You move up to the small platform. It looks to be quartz, but its prime was long, long ago, vines are up and around it, and cracks flood the floor.
+As you step up onto it, the bits and pieces of the steps crumble below your boots, and an eerie feeling falls upon you. You move near the door, but not too close,
+and can see something etched into the top of it. They seem to be words long faded away, maybe of the name of this long-forgotten place."""
