5edef9b6852d8cbfdb58d490e6861b373d6a2712
PlantCat1 <60197008+PlantCat1@users.noreply.github.com>
2021-10-11 10:19:42 -0700

    grammar fixes

--- text.py
+++ text.py
@@ -11,24 +11,24 @@ text3 = """You look around parts of the hill and surrounding area. All you find 
 of the surrounding area, and shade from a small, young hickory tree. As far as you can see, there is no sign of civilization and only more plains.
 You come to the conclusion that the platform may provide something as to why you are here."""
 
-text4 = """You step down from the platform, bits and pieces crumbling off as you do, to see if anything about this place can be found nearby.
+text4 = """You step down from the platform, bits, and pieces crumbling off as you do, to see if anything about this place can be found nearby.
 Around the hill is only grass and dirt and climbing atop the hill brings nothing but a view of the surrounding area, and shade from a small and young hickory tree.
 As far as you can see, there is no sign of civilization and only more plains. You come to the conclusion that the platform may provide something as to why you are
 here, and begin to walk towards it."""
 
 text5 = """You walk up to the faded, but white double doors. Whatever mechanism used to open the door has long fallen off leading to a small hole you can see through,
 although there is only darkness when you try to see into it. The eerie feeling continues but as this is the only way to find out what is happening,
-you try to open the heavy seemingly long forgotten doors, and they barge right open, but you are unsure if this is due to them being old and having a broken lock or
+you try to open the heavy seemingly long-forgotten doors, and they barge right open, but you are unsure if this is due to them being old and having a broken lock or
 because someone or something has unlocked it.
 You dare not step into the room that is only lit by one faint torch, which you immediately see as being weird for such an old, seemingly abandoned structure,
-but you can see from outside that after only a few units a staircase going down tempts you. Knowing there is nothing else to do you enter the room.
-Right away a deep, musty smell invades your ears. You immediately realize just how old this place truly is. The walls and ceiling are of uneven dirt like
+but you can see from the outside that after only a few units a staircase going down tempts you. Knowing there is nothing else to do you enter the room.
+Right away a deep, musty smell invades your ears. You immediately realize just how old this place truly is. The walls and ceiling are of uneven dirt-like
 material while the floor is of stone, which is unsurprisingly cracked all along. The eerie feeling, also unsurprisingly, has gotten magnitudes worse but you
 also know that you have to be here for a reason and are tempted to venture further into the unknown.
 """
 
 text6 = """You turn back out of the room but before you can exit the door swiftly shut with an echoing slam. There is a handle you use to try to move it open but
-it does not barge. You then grab the torch attached to the scone to your right and descend down the stariway.
+it does not barge. You then grab the torch attached to the scone to your right and descend down the stairway.
 """
 text7 = """You grab the torch attached to the sconce to your side. Right after the doors swiftly shut closed with an echoing slam blaring into your ears.
 You try to pull open the doors to no avail. The thought quickly dawns upon you that you’re stuck in this… place and there is nothing you can do about it but venture
@@ -36,9 +36,9 @@ forth down the stairs.
 """
 text8 = """after only a few steps, you can just about see that the stairs end to another straight corridor. Once at the bottom you assess your surroundings
 and notice the walls and ceiling are stone now, just like the floor. You creep forward, each step quietly echoing throughout the room,
-which seems to be more like a full on dungeon rather than a room to some weird abandoned structure. After what felt like much longer than it really was,
+which seems to be more like a complete dungeon rather than a room to some weird abandoned structure. After what felt like much longer than it really was,
 You see that the path abruptly ends at a stone door. You open up the old door with a deep rumble of stone pushing against stone. Inside is a square room lit with
-multiple torches along the walls, which look the same as the walls in the area previous . Directly to the other side is an open area that would be where a door is,
+multiple torches along the walls, which look the same as the walls in the area previous. Directly to the other side is an open area that would be where a door is
 but is not. To the right is a worn, wood chest with a shiny gold lock and leather wrapping the outside. But then you notice a chair with a small goblin sleeping on top of it.
 It must be a heavy sleeper given that it wasn’t awakened by the sounds of the door or anything else, but there is something to be done here.
 """
